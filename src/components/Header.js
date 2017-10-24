f5f21ee864df712589c8a3039eefa15175baab34
Fancylynn <fancylynn0@gmail.com>
2017-10-24 14:55:19 -0400

    css style test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import cssModules from "react-css-modules";
+import styles from "../styles/Header.css";
+
+// @cssModules(styles)
 
 const Header = () => {
-    return(
-        <div className="header">
-            <h1>Skin Spa Station</h1>
-        </div>
-    );
+  return(
+    <div className="header">
+      <h1 className="header-title">Skin Spa Station</h1>
+    </div>
+  );
 };
 
-export default Header;
+export default cssModules(Header, styles);
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,21 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from "react";
+import cssModules from "react-css-modules";
+import styles from "../styles/Header.css";
 
 const NavBar = props => {
-    return(
-        <nav className="navbar">
-            <button>HOME</button>
-            <button>SERVICES</button>
-            <button>LOCATIONS</button>
-            <button>RESERVATION</button>
-            <button>CONTACT US</button>
-        </nav>
-    );
+  return(
+    <nav className="navbar">
+      <button>HOME</button>
+      <button>SERVICES</button>
+      <button>LOCATIONS</button>
+      <button>RESERVATION</button>
+      <button>CONTACT US</button>
+    </nav>
+  );
 };
 
 // NavBar.propTypes = {
 //     changeDisplayMode: PropTypes.func.isRequired,
 // };
 
-export default NavBar;
+export default cssModules(NavBar, styles);
