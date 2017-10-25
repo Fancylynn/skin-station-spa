8fe146c728117f8a36d9542f431978df24014b69
Fancylynn <fancylynn0@gmail.com>
2017-10-25 11:44:17 -0400

    fix the navigation bar when scroll down

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,20 @@ import styles from "../styles/NavBar.css";
 
 const NavBar = props => {
   return(
-    <div styleName="nav-bar">
-      <ul>
-        <li>
-          <img styleName="logo" src="img/logo-inside.png" alt="logo icon"/>
-        </li>
-      </ul>
-      <div styleName="nav-links">
+    <div styleName="nav-bar-fixed">
+      <div styleName="nav-bar">
         <ul>
-          <li><a>HOME</a></li>
-          <li><a>SERVICES</a></li>
-          <li><a>LOCATIONS</a></li>
-          <li><a>RESERVATION</a></li>
-          <li><a>CONTACT US</a></li>
+          <li><img styleName="logo" src="img/logo-inside.png" alt="logo icon"/></li>
         </ul>
+        <div styleName="nav-links">
+          <ul>
+            <li><a>HOME</a></li>
+            <li><a>SERVICES</a></li>
+            <li><a>LOCATIONS</a></li>
+            <li><a>RESERVATION</a></li>
+            <li><a>CONTACT US</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
