2a52a127e353c6df7b12de2c20e4c9c72d30c9fd
Fancylynn <fancylynn0@gmail.com>
2017-10-25 23:34:25 -0400

    change margin and padding; revise nav bar css

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -54,7 +54,7 @@ class Slider extends Component {
   render() {
     const {pictures} = this.props;
     return (
-      <div styleName="slide-window" onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
         <SliderItem picture={pictures[this.state.nowLocal]}/>
       </div>
     );
@@ -62,26 +62,3 @@ class Slider extends Component {
 }
 
 export default cssModules(Slider, styles);
-{/* <SliderArrows turn={this.pictureMoveForward}/>*/}
-/* const arrowsNode = <SliderArrows turn={this.pictureMoveForward}/>;
-    const dotsNode = <SliderDots turn={this.pictureMoveForward} count={numberOfPictures} nowLocal={this.state.nowLocal}/>;
-    return (
-      <div
-          onMouseOver={pause ? this.pausePlayPictures : null} onMouseOut={pause ? this.autoPlayPictures : null}
-      >
-        <ul style={{
-          left: -100 * this.state.nowLocal + "%",
-          transitionDuration: speed + "s",
-          width: this.props.items.length * 100 + "%"
-        }}
-        >
-          {pictureDisplay}
-        </ul>
-        {arrows ? arrowsNode : null}
-        {dots ? dotsNode : null}
-      </div>
-    );*/
-
-    //  const pictureDisplay = pictures.map((picture, idx) => {
-    //   return <SliderItem picture={picture} key={idx}/>;
-    // });
