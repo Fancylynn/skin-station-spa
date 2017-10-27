6fd29d1cd01e87efa87b3847e5b07174e510f3db
Fancylynn <fancylynn0@gmail.com>
2017-10-27 15:45:14 -0400

    add most popular services left panel

--- src/components/MostPopularServices.js
+++ src/components/MostPopularServices.js
@@ -6,18 +6,23 @@ import styles from "../styles/MostPopularServices.css";
 
 const PopService = () => {
   return(
-    <div styleName="pop-services">
-      <div styleName="pop-service-facial">
-        <img styleName="facial-icon" src="img/facialIcon.png" alt="facial icon"/>
-        <p>Our popular facial services </p>
-      </div>
-      <div styleName="pop-service-body">
-        <img src="img/bodyIcon.jpg" alt="body icon"/>
-        <p>Our popular body services </p>
-      </div>
-      <div styleName="pop-service-hair">
-        <img src="img/hairIcon.jpg" alt="hair icon"/>
-        <p>Our popular hair services </p>
+    <div styleName="pop-service-container">
+      <div styleName="pop-service-row">
+        <h1 styleName="pop-service-title">Most Popular Services</h1>
+        <div styleName="pop-service-left">
+          <div styleName="pop-service-leftrow">
+            <h2>Massage</h2>
+            <p> Massage therapy for relaxation or for a variety of health conditions </p>
+          </div>
+          <div styleName="pop-service-leftrow">
+            <h2>Scrubs And Wraps</h2>
+            <p> Healthy and natural method to cleanse and exfoliate your entire body </p>
+          </div>
+          <div styleName="pop-service-leftrow">
+            <h2>Facial Add-On Mask</h2>
+            <p> Massage therapy for relaxation or for a variety of health conditions </p>
+          </div>
+        </div>
       </div>
     </div>
   );
