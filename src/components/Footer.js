31c6b878e03a69ac9b3dd9857fef78e444d6a1fb
Fancylynn <fancylynn0@gmail.com>
2017-12-13 22:05:22 -0500

    add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from "react";
 import cssModules from "react-css-modules";
 import styles from "../styles/Footer.css";
+import {Link} from "react-router";
 
 class Footer extends Component {
   render() {
@@ -8,33 +9,43 @@ class Footer extends Component {
       <div styleName="footer-container">
         <div styleName="footer-row">
           <h2> COMPANY </h2>
-          <h4> About Us </h4>
+          <Link styleName="links" to="/contact"><h4>ABOUT US</h4></Link>
           <h4> Blog </h4>
-          <h4> Location </h4>
+          <Link styleName="links" to="/location"><h4>Location</h4></Link>
           <h4> Store Policies</h4>
         </div>
         <div styleName="footer-row">
           <h2>Social</h2>
-          <div styleName="social-icon">
-            <img src="img/facebook-icon.png" alt="facebook-icon"/>
-            <h4> Facebook</h4>
-          </div>
-          <div styleName="social-icon">
-            <img src="img/twitter-icon.png" alt="twitter-icon"/>
-            <h4> Twitter</h4>
-          </div>
-          <div styleName="social-icon">
-            <img src="img/google-icon.png" alt="google-icon"/>
-            <h4> Google Plus</h4>
-          </div>
-          <div styleName="social-icon">
-            <img src="img/pinterest-icon.png" alt="pinterest-icon"/>
-            <h4> Pinterest</h4>
-          </div>
-          <div styleName="social-icon">
-            <img src="img/instagram-icon.png" alt="instagram-icon"/>
-            <h4> Instagram</h4>
-          </div>
+          <a href="https://www.facebook.com/SkinStationNY">
+            <div styleName="social-icon">
+              <img styleName="social-icon-img" src="img/facebook-icon.png" alt="facebook-icon"/>
+              <h4> Facebook</h4>
+            </div>
+          </a>
+          <a href="https://twitter.com/SkinStationUSA">
+            <div styleName="social-icon">
+              <img styleName="social-icon-img" src="img/twitter-icon.png" alt="twitter-icon"/>
+              <h4> Twitter</h4>
+            </div>
+          </a>
+          <a href="https://plus.google.com/u/0/114511122311533602619">
+            <div styleName="social-icon">
+              <img styleName="social-icon-img" src="img/google-icon.png" alt="google-icon"/>
+              <h4> Google Plus</h4>
+            </div>
+          </a>
+          <a href="https://www.pinterest.com/skinstationnyc/">
+            <div styleName="social-icon">
+              <img styleName="social-icon-img" src="img/pinterest-icon.png" alt="pinterest-icon"/>
+              <h4> Pinterest</h4>
+            </div>
+          </a>
+          <a href="https://www.instagram.com/skinstationnyc/">
+            <div styleName="social-icon">
+              <img styleName="social-icon-img" src="img/instagram-icon.png" alt="instagram-icon"/>
+              <h4> Instagram</h4>
+            </div>
+          </a>
         </div>
         <div styleName="footer-row">
           <h2>Contact</h2>
