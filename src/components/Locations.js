1447d45a5ceebd0c738018768663193952e39961
Fancylynn <fancylynn0@gmail.com>
2017-12-14 15:26:15 -0500

    change location page select style

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -45,46 +45,64 @@ class Locations extends Component {
           <div styleName="locations-container">
             <h2>All Locations</h2>
             <div styleName="store-details">
-              <div styleName="location">
-                <h3 onClick={this.onManhattan}>Manhattan</h3>
+              <div
+                  styleName={this.state.location === "manhattan" ? "location-selected" : "location"}
+                  onClick={this.onManhattan}
+              >
+                <h3>Manhattan</h3>
                 <p>113 Eldridge Street (Basement), New York, NY 1002</p>
                 <p>(Between Grand and Broome Street)</p>
                 <p>Tel: 212-343-7911</p>
                 <p>Email: skinstation.manhattan@gmail.com</p>
                 <p>Store Hours: Mon-Sun 10am-8pm</p>
               </div>
-              <div styleName="location">
-                <h3 onClick={this.onBrooklyn}>Brooklyn</h3>
+              <div
+                  styleName={this.state.location === "brooklyn" ? "location-selected" : "location"}
+                  onClick={this.onBrooklyn}
+              >
+                <h3>Brooklyn</h3>
                 <p>817 60 street (off 8th Avenue), Brooklyn, NY 11220 </p>
                 <p>Tel: 718-686-8881</p>
                 <p>Email: skinstation.brooklyn@gmail.com</p>
                 <p>Store Hours: Mon-Sun 10am-8pm</p>
               </div>
-              <div styleName="location">
-                <h3 onClick={this.onForestAve}>Forest Avenue</h3>
+              <div
+                  styleName={this.state.location === "forestave" ? "location-selected" : "location"}
+                  onClick={this.onForestAve}
+              >
+                <h3>Forest Avenue</h3>
                 <p>771 Forest Avenue, Staten Island, NY 10310</p>
                 <p>(Near Staten Island Zoo, Next to McDonald @ Broadway) </p>
                 <p>Tel: 718-442-9828</p>
                 <p>Email: skinstation771forest@gmail.com</p>
                 <p>Store Hours: Mon-Sat 9:30am-7pm, Sun 10am-5pm </p>
               </div>
-              <div styleName="location">
-                <h3 onClick={this.onOldTown}>Old Town</h3>
+              <div
+                  styleName={this.state.location === "oldtown" ? "location-selected" : "location"}
+                  onClick={this.onOldTown}
+              >
+                <h3>Old Town</h3>
                 <p>205 Old Town Road, Staten Island, NY 10305</p>
                 <p>(Off Hylan Blvd)</p>
                 <p>Tel: 718-980-1500</p>
                 <p>Email: skinstation.oldtown@gmail.com</p>
                 <p>Store Hours: Mon-Sat 9:30am-7pm, Sun 10am-5pm </p>
               </div>
-              <div styleName="location">
-                <h3 onClick={this.onQueens}>Queens</h3>
+              <div
+                  styleName={this.state.location === "queens" ? "location-selected" : "location"}
+                  onClick={this.onQueens}
+              >
+                <h3>Queens</h3>
                 <p>36-16 Union Street, 2/F, Flushing, NY 11354</p>
                 <p>Tel: 718-961-4505</p>
                 <p>Email: skinstationqueens@gmail.com</p>
                 <p>Store Hours: Mon-Sun 10am-8pm </p>
               </div>
-              <div styleName="location">
-                <h3 onClick={this.onYonkers}>Yonkers</h3>
+              <div
+                  styleName={this.state.location === "yonkers" ? "location-selected" : "location"}
+                  onClick={this.onYonkers}
+              >
+                <h3>Yonkers</h3>
                 <p>2500 Central Park Avenue, Yonkers, NY 10710 </p>
                 <p>(Next to Movie Theater, Behind Best Buy) </p>
                 <p>Tel: 914-395-3260 | 914-395-3256 </p>
