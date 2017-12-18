8b3413c5fae33169bd25f09189c5d916038efecd
Fancylynn <fancylynn0@gmail.com>
2017-12-18 17:24:48 -0500

    add contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,12 +5,15 @@ import cssModules from "react-css-modules";
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div styleName="contact-container">
+        <h1 styleName="contact-title">Contact</h1>
+        <p styleName="contact-content">Please feel free to contact us by filling out the form below and we will get back as soon as possible!</p>
         <form styleName="contact-form">
-                Name <input type="text" name="name"/>
-                Email <input type="email" name="email"/>
-                Phone Number <input type="tel" name="phone-number"/>
-                Message <input type="text" name="message"/>
+                Your Name (required) <input styleName="input-text-box" type="text" name="name" size="40"/>
+                Your Email (required) <input styleName="input-text-box"type="email" name="email"/>
+                Subject <input styleName="input-text-box" type="text" name="phone-number"/>
+                Your Message <textarea styleName="input-textarea-box" rows="5" cols="50"/>
+                <input styleName="contact-button" type="submit" value="Submit"/>
         </form>
       </div>
     );
