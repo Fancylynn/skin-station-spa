25e0e03628a16902c0583ffadae7b53e246744c3
Fancylynn <fancylynn0@gmail.com>
2018-02-15 00:12:00 -0500

    add bootstrap to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,24 @@
-import React, {PropTypes} from "react";
+import React, {PropTypes, Component} from "react";
 import cssModules from "react-css-modules";
 import styles from "../styles/NavBar.css";
 import {Link} from "react-router";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const NavBar = props => {
-  return(
+class NavBar extends Component {
+  /* return(
     <div styleName="nav-bar-fixed">
+      <Button color="danger">Danger!</Button>
       <div styleName="nav-bar">
         <ul>
           <li><img styleName="logo" src="img/logo-inside.png" alt="logo icon"/></li>
@@ -22,8 +35,52 @@ const NavBar = props => {
         </div>
       </div>
     </div>
-  );
-};
+  );*/
+
+    state = {
+      isOpen: false
+    };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div styleName="nav-bar-fixed">
+        <Navbar color="faded" light expand="md">
+          <NavbarBrand href="/">
+            <img styleName="logo" src="img/logo-inside.png" alt="logo icon"/>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle}/>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/">Home</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/services">Services</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/location">Locations</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/reservation">Reservation</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/aboutUs">About Us</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link styleName="links" to="/signup">Sign In</Link></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 // NavBar.propTypes = {
 //     changeDisplayMode: PropTypes.func.isRequired,
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import {Provider} from "react-redux";
 import {createStore} from "redux";
 import {render} from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
 // import Homepage from "./containers/Homepage";
+
 
 // const store = createStore(
 // 	window.devToolsExtension && window.devToolsExtension()
