aa6e1b02f210068588a8e4d422ec9da2570a9b49
Fancylynn <fancylynn0@gmail.com>
2018-02-15 00:12:48 -0500

    add bootstrap

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -121,8 +121,10 @@ module.exports = {
       // in development "style" loader enables hot editing of CSS.
       {
         test: /\.css$/,
-        loader: 'style!css?modules!postcss'
+        loader: 'style!css?modules!postcss',
+        exclude: /node_modules/
       },
+      { test: /\.css$/, loader: 'style-loader!css-loader', include: /node_modules/ },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
