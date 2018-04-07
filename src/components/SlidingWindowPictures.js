e0fc42882ed9a207c20416a691ec7de513917584
Fancylynn <fancylynn0@gmail.com>
2018-04-07 19:31:15 -0400

    change homepage slider

--- src/components/SlidingWindowPictures.js
+++ src/components/SlidingWindowPictures.js
@@ -1,32 +1,99 @@
-import React from "react";
-// import {render} from "react-dom";
-import Slider from "./Slider";
+import React, {Component} from "react";
+// import Slider from "./Slider";
 import cssModules from "react-css-modules";
 import styles from "../styles/SlidingWindowPictures.css";
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from "reactstrap";
 
 const IMAGE_DATA = [
   {
     src: "img/coverpage1.png",
-    alt: "images-1"
+    altText: "images-1",
+    caption: "Slide 1"
   },
   {
     src: "img/coverpage2.jpg",
-    alt: "images-2"
+    altText: "images-2",
+    caption: "Slide 2"
   },
   {
     src: "img/coverpage3.jpg",
-    alt: "images-3"
+    altText: "images-3",
+    caption: "Slide 3"
   }
 ];
 
-const SlidingWindowPictures = () => {
-  return(
-    <div>
-      <Slider
-          pictures={IMAGE_DATA}
-      />
-    </div>
-  );
-};
+class SlidingWindowPictures extends Component {
+  state = {
+    activeIndex: 0,
+    paused: false,
+    width: window.innerWidth
+  };
+
+  componentDidMount() {
+     this.updateWindowDimensions();
+     window.addEventListener("resize", this.updateWindowDimensions);
+   }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({width: window.innerWidth});
+  }
+
+  pause = () => {
+    this.setState({paused: true});
+  }
+
+  cycle = () => {
+    this.setState({paused: false});
+  }
+
+  next = () =>{
+    const nextIndex = this.state.activeIndex === IMAGE_DATA.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({activeIndex: nextIndex});
+  }
+
+  previous = () => {
+    if (this.state.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? IMAGE_DATA.length - 1 : this.state.activeIndex - 1;
+    this.setState({activeIndex: nextIndex});
+  }
+
+  goToIndex = (newIndex) => {
+    this.setState({activeIndex: newIndex});
+  }
+
+  render() {
+    const slides = IMAGE_DATA.map((item, idx) => {
+      return (
+        <CarouselItem key={idx} src={item.src} altText={item.altText}>
+          <img width={this.state.width}src={item.src} alt={item.altText}/>
+          {/* <CarouselCaption captionText={item.caption} captionHeader={item.caption}/>*/}
+        </CarouselItem>
+      );
+    });
+
+    return (
+        <Carousel
+            next={this.next}
+            previous={this.previous}
+            activeIndex={this.state.activeIndex}
+            paused={this.state.paused}
+            hoverStart={this.pause}
+            hoverEnd={this.cycle}
+        >
+          {slides}
+        </Carousel>
+    );
+  }
+}
 
 export default cssModules(SlidingWindowPictures, styles);
