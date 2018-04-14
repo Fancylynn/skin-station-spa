4aee242976730db6dfcddf2cf9dd2d3a42ee9599
Fancylynn <fancylynn0@gmail.com>
2018-04-13 21:10:47 -0400

    remove backicon when the screen width smaller than ipad

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,30 @@ import BackIcon from "./BackIcon";
 import React, {Component} from "react";
 
 class Layout extends Component {
+  state = {
+    width: window.innerWidth
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => {
+    this.setState({width: window.innerWidth});
+  }
+
   render() {
     return (
       <div>
         <NavBar/>
         {this.props.children}
         <Footer/>
-        <BackIcon/>
+        {this.state.width > 768 && <BackIcon/>}
       </div>
     );
   }
